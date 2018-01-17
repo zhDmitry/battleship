517fe2a70a87e5e556ad88290b224c49b597b093
Dmitry Zherebko <kraken@live.ru>
2018-01-17 22:00:12 +0200

    Updates

--- a90a9f9aca83f418000d25e90bbd828e.js
+++ a90a9f9aca83f418000d25e90bbd828e.js
@@ -146,7 +146,7 @@ function Module() {
 module.bundle.Module = Module;
 
 if (!module.bundle.parent) {
-  var ws = new WebSocket('ws://localhost:51475/');
+  var ws = new WebSocket('ws://localhost:52819/');
   ws.onmessage = function(event) {
     var data = JSON.parse(event.data);
 
